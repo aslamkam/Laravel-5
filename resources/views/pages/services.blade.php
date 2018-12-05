bb7bd7d568484a8211d3ae382da06f726e83285a
Kamal <crispevening@gmail.com>
2018-12-05 13:19:54 -0500

    Experiments with Laravel 5

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/app')
+@section('content')
+        <h1>Welcome to {{$title}}</h1>
+        @if(count($services) > 0)
+        <ul>
+            @foreach ($services as $service)
+                    <li>{{$service}}</li>
+            @endforeach
+        </ul>
+        @endif
+@endsection
